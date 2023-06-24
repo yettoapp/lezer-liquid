0c7a493a76ff70125e153d3ca46d9110c5e23be3
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2023-06-23 18:46:07 -0700

    add dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,13 @@
-import {nodeResolve} from "@rollup/plugin-node-resolve"
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "./src/parser.js",
   output: [
     { format: "cjs", file: "./dist/index.cjs" },
-    { format: "es", file: "./dist/index.es.js" }
+    { format: "es", file: "./dist/index.es.js" },
   ],
   external(id) {
-    return !/^[\.\/]/.test(id)
+    return !/^[\.\/]/.test(id);
   },
-  plugins: [
-    nodeResolve()
-  ]
-}
+  plugins: [nodeResolve()],
+};
